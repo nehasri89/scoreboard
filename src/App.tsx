e0a075c5cc50b430eea8ee9c08a22453800a4b8e
Neha Srivastava <neha.srivastava8@gmail.com>
2023-05-23 07:29:30 +0100

    Communicating with websocket server

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useEffect, useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const payLoad = {
+  cmd: "start",
+  opts: {
+    speed: 60,
+  },
+};
+
+const App = () => {
+  const [data, setData] = useState();
+  // const [previousSets, setPreviousSets] = useState()
+
+  useEffect(() => {
+    const ws = new WebSocket("ws://localhost:8081");
+
+    //add an event listener when connection is established
+    ws.onopen = (event) => {
+      console.log("WebSocket is connected3.");
+      ws.send(JSON.stringify(payLoad));
+    };
+
+    //add an event listener for the message event
+    ws.onmessage = (event) => {
+      console.log(` Data received from server2: ${event.data}`);
+      setData(event.data);
+    };
+
+    //add an event listener when error occurs
+    ws.onerror = (error) => {
+      console.log("WebSocket Error: " + error);
+    };
+
+    // add an event listener when socket closes.
+    ws.onclose = (event) => {
+      console.log("Disconnected from WebSocket.");
+    };
+
+    //close the connection when app unmounts
+    return () => ws.close();
+  }, []);
+
+  return <div>HELLO{data}</div>;
+};
 
 export default App;
